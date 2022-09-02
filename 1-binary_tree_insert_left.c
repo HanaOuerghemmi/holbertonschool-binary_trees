a4ce43a7646af13acf9a70034a31df67a015df7e
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-02 16:39:00 +0100

    insert in the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,23 +8,25 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tmp;
-	(void)value;
+
 
 	if (parent == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	tmp = malloc(sizeof(binary_tree_t));
+	tmp = binary_tree_node(parent, value); /** create a node in a binary tree*/
 
 	if (tmp == NULL)
 	{
 		return (NULL);
 	}
+
 	if (parent->left != NULL)
 	{
 		tmp->left = parent->left;
 		parent->left->parent = tmp;
 	}
+
 	parent->left = tmp;
 	return (tmp);
 }
