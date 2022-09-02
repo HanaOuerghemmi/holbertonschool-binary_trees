c8fb9a5ebbe1823aa03c394108629c387a9ef670
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-02 20:58:37 +0100

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,17 +9,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t l, r;
 
 	if (tree == NULL)
-	{
+
 		return (0);
-	}
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
+
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
-	if (l > r)
-	{
+
+	if (l >= r)
 		return (l + 1);
-	}
 	else
-	{
 		return (r + 1);
-	}
+
 }
+
