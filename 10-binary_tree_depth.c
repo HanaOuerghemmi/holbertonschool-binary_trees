6b7260ea5b580c97eba30d723ff00cd3db1a5caf
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-02 21:09:26 +0100

    height

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height -a function that measures the height of a binary tree
+ * @tree: tree
+ * Return: a pointer to the new node or null on failleure
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (tree == NULL)
+
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+
+	depth = binary_tree_height(tree->parent);
+	depth++;
+	return (depth);
+
+}
+
