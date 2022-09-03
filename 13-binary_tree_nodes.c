bd09797b757be5df2615b85a1251c833227d63f6
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-03 17:55:23 +0100

    counts the nodes with a least 1 child:alien:

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - function that counts the nodes
+ * with at least 1 child in a binary tree
+ * @tree: the tree
+ * Return: number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (tree->right || tree->left)
+			return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
+
+		return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left));
+	}
+	else
+		return (0);
+
+}
